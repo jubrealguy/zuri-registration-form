8a33494c993c3ea080781086895f65520403715a
Jubreel <whizkidjubrealguy@gmail.com>
2022-06-19 23:39:23 +0100

    perfected

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-const firstName = document.querySelector("#fname");
-const lastName = document.querySelector('#lname')
-const email = document.querySelector("#email");
-const password = document.querySelector("#pass");
 const btn = document.querySelector("#submit");
 
 const error_fname = document.querySelector("#error-fname");
 const error_lname = document.querySelector("#error-lname");
 const error_email = document.querySelector("#error-email");
 const error_pass = document.querySelector("#error-pass");
+
+
+const error = document.querySelectorAll(".err")
 
 const invalidFname = document.querySelector(".one");
 const invalidLname = document.querySelector(".two");
@@ -22,22 +21,11 @@ const invalidPassword = document.querySelector(".four");
     password: ""
 };
 
-
-firstName.addEventListener("input", (e) => {
-    formObj.fname = e.target.value;
-})
-
-lastName.addEventListener("input", (e) => {
-    formObj.lname = e.target.value;
-})
-
-email.addEventListener("input", (e) => {
-    formObj.email = e.target.value;
-})
-
-password.addEventListener("input", (e) => {
-    formObj.password = e.target.value;
-})
+Object.keys(formObj).forEach((dataKey) => {
+    document.querySelector(`#${dataKey}`).addEventListener("input", (e) => {
+        formObj[dataKey] = e.target.value;
+    });
+});
 
 btn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -45,22 +33,26 @@ btn.addEventListener("click", (e) => {
     if (!formObj.fname) {
         error_fname.textContent = "First Name cannot be empty";
         invalidFname.className = "invalid";
+        error[0].className = "error"
     }
 
     if (!formObj.lname) {
         error_lname.textContent = "Last Name cannot be empty";
         invalidLname.className = "invalid";
+        error[1].className = "error"
     }
 
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!formObj.email.match(mailformat)) {
         error_email.textContent = "Looks like this is not an email";
         invalidEmail.className = "invalid";
+        error[2].className = "error"
     }
 
     if (!formObj.password) {
         error_pass.textContent = "password cannot be empty";
         invalidPassword.className = "invalid";
+        error[3].className = "error"
     }
 
     return true;
